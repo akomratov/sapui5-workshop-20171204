01c92243792062bffee97a4cb36e2cfb14d1f41b
akomratov <a.komratov@yandex.ru>
2017-12-05 15:19:51 +0300

    MessageToast added to onPress event handler

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,15 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-	], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+	], function(Controller, MessageToast) {
 		"use strict";
 		return Controller.extend("workshop.wt.controller.App", {
 			onShowHello: function() {
-				alert("Hello Button was pressed");
+				//alert("Hello Button was pressed");
+				MessageToast.show("Hello Button was pressed", { 
+					duration: 500, 
+					animationDuration: 2000,
+					animationTimingFunction: "ease-in-out" });
 			}
 		});
 });
