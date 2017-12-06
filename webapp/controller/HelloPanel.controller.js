56bacafdb03fd9ae5e6c9864a25be97c003e2bd9
akomratov <a.komratov@yandex.ru>
2017-12-06 12:41:43 +0300

    SELF task (add button to open Dialog)

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,9 +1,15 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-	], function(Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"sap/m/Dialog",
+	"sap/m/FormattedText",
+	"sap/m/Button"
+	], function(Controller, MessageToast, Dialog, FormattedText, Button) {
 		"use strict";
 		return Controller.extend("workshop.wt.controller.HelloPanel", {
+			
+			oDialog: null,
+			
 			onShowHello: function() {
 				//alert("Hello Button was pressed");
 				
@@ -18,6 +24,29 @@ sap.ui.define([
 					animationTimingFunction: "ease-in-out" });
 			},
 			onInit: function() {
+			},
+			onShowDialog: function() {
+				if(!this.oDialog) {
+					this.oDialog = new Dialog({
+						title: "{i18n>dialogTitle}",
+						content: new FormattedText({
+							htmlText: "{i18n>dialogText}"
+						}),
+						beginButton: new Button({
+							text: 'Close',
+							press: function () {
+								this.oDialog.close();
+							}.bind(this)
+						})
+					});
+					this.oDialog.addStyleClass("sapUiSmallMargin");
+					this.oDialog.addStyleClass("sapThemeHighlight-asColor");
+	
+					//to get access to the global model
+					this.getView().addDependent(this.oDialog);
+				}
+	
+				this.oDialog.open();
 			}
 		});
 });
